24a9c02978b0824822c12c55fb631deffd9500d2
Yiran Sheng <shengyiran@gmail.com>
2018-10-19 02:46:19 -0700

    Shuffle apis and use transmute

--- src/world.rs
+++ src/world.rs
@@ -1,13 +1,12 @@
-use std::convert::Into;
-use std::default::Default;
-use std::ops::Add;
+use std::convert::{From, Into};
+use std::mem::transmute;
 
-#[derive(Copy, Clone, Eq, PartialEq)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum Direction {
-    North = 1,
-    South = 2,
-    East = 3,
-    West = 4,
+    North = 0b1000_0000,
+    South = 0b1000_0001,
+    East = 0b1000_0010,
+    West = 0b1000_0011,
 }
 
 impl Direction {
@@ -29,7 +28,7 @@ pub enum Tile {
     OutOfBound,
 }
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq, Default)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub struct Block {
     raw: u8,
 }
@@ -39,10 +38,12 @@ impl Block {
         Block { raw: 0 }
     }
     pub fn food() -> Self {
-        Block { raw: 5 }
+        // second bit on: food
+        Block { raw: 0b0100_0000 }
     }
     pub fn out_of_bound() -> Self {
-        Block { raw: 255 }
+        // first two bits off: OOB
+        Block { raw: 0b0011_1111 }
     }
     pub fn from_raw(raw: u8) -> Self {
         Block { raw }
@@ -51,41 +52,41 @@ impl Block {
         self.raw
     }
     pub fn is_snake(self) -> bool {
-        self.raw > 0 && self.raw <= 4
+        // first bit is 1
+        self.raw & 0b1000_0000 != 0
+    }
+    #[inline(always)]
+    unsafe fn into_direction(self) -> Direction {
+        // keep only lower 2 bits, then turn on first bit
+        let bits = self.raw & 0b0000_0011 | 0b1000_0000;
+        transmute(bits)
     }
 }
 
-impl Into<Tile> for Block {
-    fn into(self) -> Tile {
-        match self.raw {
-            0 => Tile::Empty,
-            1 | 2 | 3 | 4 => Tile::Snake,
-            5 => Tile::Food,
-            _ => Tile::OutOfBound,
-        }
-    }
-}
+// conversions
 
 impl Into<Block> for u8 {
     fn into(self) -> Block {
         Block::from_raw(self)
     }
 }
-impl Into<Direction> for u8 {
-    fn into(self) -> Direction {
-        match self {
-            1 => Direction::North,
-            2 => Direction::South,
-            3 => Direction::East,
-            4 => Direction::West,
-            _ => panic!("not a direction"),
+
+impl From<Block> for Tile {
+    fn from(b: Block) -> Tile {
+        match b.raw {
+            0 => Tile::Empty,
+            b if (b & 0b1000_0000) != 0 => Tile::Snake,
+            b if (b & 0b0100_0000) != 0 => Tile::Food,
+            _ => Tile::OutOfBound,
         }
     }
 }
 
-impl Into<Direction> for Block {
-    fn into(self) -> Direction {
-        self.raw.into()
+impl From<Block> for Direction {
+    fn from(b: Block) -> Direction {
+        debug_assert!(b.is_snake());
+
+        unsafe { b.into_direction() }
     }
 }
 impl From<Direction> for Block {
@@ -96,9 +97,11 @@ impl From<Direction> for Block {
 
 pub struct Board {
     blocks: Vec<Block>,
-    width: u32,
-    height: u32,
+    width: i32,
+    height: i32,
 }
+
+// game states
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Default)]
 pub struct Coordinate {
@@ -107,6 +110,12 @@ pub struct Coordinate {
 }
 
 impl Coordinate {
+    pub fn from_usize(x: usize, y: usize) -> Self {
+        Coordinate {
+            x: x as i32,
+            y: y as i32,
+        }
+    }
     fn move_towards(self, dir: Direction) -> Self {
         let Coordinate { x, y } = self;
         match dir {
@@ -119,17 +128,17 @@ impl Coordinate {
 }
 
 impl Board {
-    fn new(width: u32, height: u32) -> Self {
+    fn empty(width: u32, height: u32) -> Self {
         Board {
-            width,
-            height,
+            width: width as i32,
+            height: height as i32,
             blocks: vec![Block::empty(); (width * height) as usize],
         }
     }
     fn get_block(&self, coord: Coordinate) -> Block {
         let Coordinate { x, y } = coord;
-        let width = self.width as i32;
-        let height = self.height as i32;
+        let width = self.width;
+        let height = self.height;
 
         if x < 0 || x >= width || y < 0 || y >= height {
             Block::out_of_bound()
@@ -137,10 +146,15 @@ impl Board {
             self.blocks[(y * width + x) as usize]
         }
     }
+    fn set_block(&mut self, coord: Coordinate, b: Block) {
+        if let Some(block) = self.get_block_mut(coord) {
+            *block = b;
+        }
+    }
     fn get_block_mut(&mut self, coord: Coordinate) -> Option<&mut Block> {
         let Coordinate { x, y } = coord;
-        let width = self.width as i32;
-        let height = self.height as i32;
+        let width = self.width;
+        let height = self.height;;
 
         if x < 0 || x >= width || y < 0 || y >= height {
             None
@@ -148,28 +162,17 @@ impl Board {
             self.blocks.get_mut((y * width + x) as usize)
         }
     }
-    fn set_block<B: Into<Block>>(&mut self, coord: Coordinate, b: B) {
-        if let Some(block) = self.get_block_mut(coord) {
-            *block = b.into();
-        }
-    }
-
-    #[inline]
-    pub fn get_tile(&self, coord: Coordinate) -> Tile {
-        self.get_block(coord).into()
-    }
-    #[inline]
-    pub fn get_snake_direction(&self, coord: Coordinate) -> Direction {
-        self.get_block(coord).into()
-    }
 
     fn get_prev_snake_block(&self, coord: Coordinate) -> Option<Block> {
         let b = self.get_block(coord);
+
         if b.is_snake() {
-            let dir: Direction = b.into();
+            let dir = Direction::from(b);
             let dir = dir.opposite();
+
             let prev_coord = coord.move_towards(dir);
             let prev_block = self.get_block(prev_coord);
+
             if prev_block.is_snake() {
                 Some(prev_block)
             } else {
@@ -181,9 +184,13 @@ impl Board {
     }
     fn get_next_snake_block(&self, coord: Coordinate) -> Option<Block> {
         let b = self.get_block(coord);
+
         if b.is_snake() {
-            let next_coord = coord.move_towards(b.into());
+            let dir = Direction::from(b);
+
+            let next_coord = coord.move_towards(dir);
             let next_block = self.get_block(next_coord);
+
             if next_block.is_snake() {
                 Some(next_block)
             } else {
@@ -209,22 +216,14 @@ enum UpdateError {
 type Result<T> = ::std::result::Result<T, UpdateError>;
 
 impl World {
-    fn set_snake_direction(&mut self, dir: Direction) {
-        let block = self.board.get_block_mut(self.head).unwrap();
-        *block = Block::from(dir)
-    }
-    fn set_block<B: Into<Block>>(&mut self, coord: Coordinate, b: B) {
-        let block = self.board.get_block_mut(coord).unwrap();
-        *block = b.into()
-    }
-    fn set_direction(&mut self, dir: Direction) {
+    pub fn set_direction(&mut self, dir: Direction) {
         let head = self.head;
         self.set_block(head, dir);
     }
-    fn update(&mut self) -> Result<()> {
-        let head_dir = self.board.get_snake_direction(self.head);
+    pub fn update(&mut self) -> Result<()> {
+        let head_dir: Direction = self.board.get_block(self.head).into();
         let next_head = self.head.move_towards(head_dir);
-        let next_head_tile = self.board.get_tile(next_head);
+        let next_head_tile: Tile = self.board.get_block(next_head).into();
 
         match next_head_tile {
             Tile::OutOfBound => Err(UpdateError::OutOfBound),
@@ -234,7 +233,7 @@ impl World {
                 self.head = next_head;
 
                 let tail = self.tail;
-                let tail_dir = self.board.get_snake_direction(self.tail);
+                let tail_dir = self.board.get_block(self.tail).into();
                 let next_tail = self.tail.move_towards(tail_dir);
 
                 self.set_block(tail, Block::empty());
@@ -247,6 +246,11 @@ impl World {
                 Ok(())
             }
         }
+    }
+
+    #[inline]
+    fn set_block<B: Into<Block>>(&mut self, coord: Coordinate, b: B) {
+        self.board.set_block(coord, b.into())
     }
 }
 
@@ -281,27 +285,24 @@ mod test_utils {
     }
 
     impl World {
+        #[allow(dead_code)]
         pub fn from_ascii(s: &str) -> Self {
             let height = s.lines().count() as u32;
             let width = s.lines().next().unwrap().chars().count() as u32;
 
-            let mut board = Board::new(width, height);
+            let mut board = Board::empty(width, height);
 
             for (y, line) in s.lines().enumerate() {
                 for (x, c) in line.chars().enumerate() {
-                    let x = x as i32;
-                    let y = y as i32;
-                    board.set_block(Coordinate { x, y }, c);
+                    board.set_block(Coordinate::from_usize(x, y), c.into());
                 }
             }
 
             let mut head = Coordinate { x: 0, y: 0 };
             let mut tail = Coordinate { x: 0, y: 0 };
 
-            for x in 0..width {
-                for y in 0..height {
-                    let x = x as i32;
-                    let y = y as i32;
+            for x in 0..board.width {
+                for y in 0..board.height {
                     let coord = Coordinate { x, y };
                     let b = board.get_block(coord);
                     if b.is_snake() {
@@ -323,12 +324,10 @@ mod test_utils {
         fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
             for y in 0..self.board.height {
                 for x in 0..self.board.width {
-                    let x = x as i32;
-                    let y = y as i32;
                     let coord = Coordinate { x, y };
                     write!(f, "{}", self.board.get_block(coord))?;
                 }
-                write!(f, "\n");
+                write!(f, "\n")?;
             }
             Ok(())
         }
@@ -339,6 +338,30 @@ mod test_utils {
 mod tests {
     use super::test_utils::*;
     use super::*;
+
+    #[test]
+    fn test_converion() {
+        for x in 0..u8::max_value() {
+            let b = Block::from_raw(x);
+            let tile = Tile::from(b);
+
+            if b.is_snake() {
+                assert_eq!(tile, Tile::Snake);
+            }
+
+            // make sure every u8 bit pattern result in a valid Direction
+            // ...that is, not getting SIGIL or other UB
+            unsafe {
+                let dir = b.into_direction();
+                assert!(
+                    dir == Direction::North
+                        || dir == Direction::South
+                        || dir == Direction::East
+                        || dir == Direction::West
+                );
+            }
+        }
+    }
 
     #[test]
     fn test_game() {
