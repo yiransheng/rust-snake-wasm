c90d2d8c706f9dd64792753bb850da521c87c04d
Yiran Sheng <shengyiran@gmail.com>
2018-10-18 22:24:11 -0700

    Clean up all

--- rust/build.sh
+++
@@ -1,2 +0,0 @@
-cargo  +nightly build --target wasm32-unknown-unknown --release
-wasm-gc target/wasm32-unknown-unknown/release/snake_wasm.wasm target/wasm32-unknown-unknown/release/snake_wasm.small.wasm
