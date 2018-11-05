b4a332d8b8f9748faa85ef28909c47cf5d347ea5
Yiran Sheng <shengyiran@gmail.com>
2018-11-05 11:43:30 -0800

    Add tests

--- src/world/tests.rs
+++ src/world/tests.rs
@@ -138,6 +138,112 @@ fn test_bounding() {
     assert_eq!(&afterwards, &world.grid.to_string());
 }
 
+impl<R: Rng, BB: BoundingBehavior> World<R, BB> {
+    fn head_dir(&self) -> Direction {
+        self.get_block(self.head).snake().unwrap()
+    }
+    fn with_direction<F>(&mut self, dir: Direction, f: F)
+    where
+        F: Fn(Direction, Direction),
+    {
+        let prev_dir = self.head_dir();
+        self.set_direction(dir).unwrap();
+        let next_dir = self.head_dir();
+
+        f(prev_dir, next_dir);
+
+        let head = self.head;
+        self.set_block(head, Block::Snake(prev_dir));
+    }
+}
+
+#[test]
+fn test_set_direction_one_block_snake() {
+    let snake_string = indoc!(
+        "
+        ..........
+        ....>.....
+        ..........
+        ....*.....
+        .........."
+    );
+    let mut world: World<SmallRng, Bounding> = World::from_ascii(snake_string);
+
+    world.with_direction(Direction::North, |_prev_dir, next_dir| {
+        assert_eq!(next_dir, Direction::North);
+    });
+
+    world.with_direction(Direction::East, |_prev_dir, next_dir| {
+        assert_eq!(next_dir, Direction::East);
+    });
+
+    world.with_direction(Direction::South, |_prev_dir, next_dir| {
+        assert_eq!(next_dir, Direction::South);
+    });
+
+    world.with_direction(Direction::West, |_prev_dir, next_dir| {
+        assert_eq!(next_dir, Direction::West);
+    });
+}
+
+#[test]
+fn test_set_direction_straight() {
+    let snake_string = indoc!(
+        "
+        ..........
+        .>>>>.....
+        ..........
+        ....*.....
+        .........."
+    );
+    let mut world: World<SmallRng, Bounding> = World::from_ascii(snake_string);
+
+    world.with_direction(Direction::North, |_prev_dir, next_dir| {
+        assert_eq!(next_dir, Direction::North);
+    });
+
+    world.with_direction(Direction::East, |_prev_dir, next_dir| {
+        assert_eq!(next_dir, Direction::East);
+    });
+
+    world.with_direction(Direction::South, |_prev_dir, next_dir| {
+        assert_eq!(next_dir, Direction::South);
+    });
+
+    world.with_direction(Direction::West, |prev_dir, next_dir| {
+        assert_eq!(next_dir, prev_dir);
+    });
+}
+
+#[test]
+fn test_set_direction_turning() {
+    let snake_string = indoc!(
+        "
+        ..........
+        .>>>^.....
+        ..........
+        ....*.....
+        .........."
+    );
+    let mut world: World<SmallRng, Bounding> = World::from_ascii(snake_string);
+
+    world.with_direction(Direction::North, |_prev_dir, next_dir| {
+        assert_eq!(next_dir, Direction::North);
+    });
+
+    world.with_direction(Direction::East, |_prev_dir, next_dir| {
+        assert_eq!(next_dir, Direction::East);
+    });
+
+    world.with_direction(Direction::South, |_prev_dir, next_dir| {
+        assert_eq!(next_dir, Direction::South);
+    });
+
+    world.with_direction(Direction::West, |prev_dir, next_dir| {
+        assert_eq!(next_dir, prev_dir);
+    });
+}
+
 #[test]
 fn test_set_direction_when_consuming_does_not_kill_it() {
     // gh issue: https://github.com/yiransheng/rust-snake-wasm/issues/11
